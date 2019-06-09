a0e05cd27d123c4cb6369fa9a8185c74b3e65757
Angel Castro <angel90650@gmail.com>
2019-06-08 17:40:35 -0700

    add food entry model

--- models/food.js
+++ models/food.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose')
+
+const foodSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    calories: {type: Number, required: true},
+    protien: Number,
+    carbs: Number,
+    fat: Number
+})
+
+module.exports = mongoose.model('Food',foodSchema)

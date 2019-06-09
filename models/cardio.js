72b5b1350a94a6e18d7dd97321e6fe9d6f353487
Angel Castro <angel90650@gmail.com>
2019-06-08 17:41:49 -0700

    add cardio exercise model

--- models/cardio.js
+++ models/cardio.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose')
+
+const cardioSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    duration: Number,
+})
+
+module.exports = mongoose.model('Cardio', cardioSchema)

da2286c77f7f22950b282ced5b33c0b83c37f416
Angel Castro <angel90650@gmail.com>
2019-06-08 17:38:17 -0700

    add strength exercise model

--- models/strength.js
+++ models/strength.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose')
+
+const strengthSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    weight: Number,
+    duration: Number,
+    sets: Number,
+    reps: Number
+})
+
+module.exports = mongoose.model('Strength', strengthSchema)

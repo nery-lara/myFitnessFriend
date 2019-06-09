dbc32544ff0db05e5fc7bc308f3cfb5913b6a8d2
Angel Castro <angel90650@gmail.com>
2019-06-08 18:30:48 -0700

    add login and signup endpoints that use user models

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 'use strict'
+const express = require('express')
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 module.exports = function(app) {
     app.get('/', (req, res) => {
@@ -8,9 +12,82 @@ module.exports = function(app) {
         res.send('this will be about')
     })
     app.get('/login', (req, res) => {
-
+        User.find({email: req.body.email}).exec().then(user => {
+            if(user.length < 1) {
+                return res.status(401).json({
+                    message: 'authentication failed'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err) {
+                    return res.status(401).json({
+                        message: 'authentication failed'
+                    }) 
+                }
+                if(result) {
+                    return res.status(200).json({
+                        message: 'Authentication successful'
+                    })
+                }
+                res.status(401).json({
+                    message: 'authentication failed'
+                }) 
+            })
+        })
+        
     })
-    app.get('/signup', (req, res) => {
-
+    app.post('/signup', (req, res) => {
+        console.log(req.body)
+        User.find({email: req.body.email}).exec().then(user =>{
+            if(user.length >= 1){
+                return res.status(422).json({
+                    message: 'email exists'
+                })
+            }else{
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        })
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        })
+                        user.save().then(result => {
+                            console.log(result)
+                            res.status(201).json({
+                                message: 'user created'
+                            })
+                        }).catch(err => {
+                            console.log(err)
+                            res.status(500).json({
+                                error: err
+                            })
+                        })
+                    }
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
     })
+
+    app.delete("/:userId", (req, res) => {
+        User.remove({_id: req.params.userId}).exec().then(result => {
+            res.status(200).json({
+                message: "user deleted"
+            })
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+    })
+    
 }
--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express()
 const path = require('path')
 const logger = require('./middleware/logger')
 const routes = require('./routes/index.js')
-const mongo = require('mongodb')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
-var MongoClient = mongo.MongoClient
+
 var dbUrl = 'mongodb://localhost:27017/fitnessfriend'
-MongoClient.connect(dbUrl, { useNewUrlParser: true }, (err, db) => {
-    db.db('fitnessfrient').createCollection('users', (err, res) => {
-        console.log('user db created')
-        db.close()
-    })
-})
+mongoose.connect(dbUrl, {useNewUrlParser: true})
 app.use(logger)
+app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')

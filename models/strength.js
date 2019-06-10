58538e1edd9e4452b6033839a8d9eb13a5746a61
Angel Castro <angel90650@gmail.com>
2019-06-09 22:12:38 -0700

    strength exercises stored in db by form

--- models/strength.js
+++ models/strength.js
@@ -4,9 +4,9 @@ const strengthSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
     weight: Number,
-    duration: Number,
     sets: Number,
     reps: Number,
+    time: String,
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
 

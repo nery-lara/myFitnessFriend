3a5616b2583a7068eda1b8d7612ec9d81d17e27a
Angel Castro <angel90650@gmail.com>
2019-06-10 03:03:40 -0700

    add ability to switch days on daily tab

--- public/home.js
+++ public/home.js
@@ -2,44 +2,74 @@ var todayDate = new Date()
 
 localStorage.setItem('date', todayDate.toISOString())
 console.log(localStorage.getItem('date'))
+function loadData() {
+    $.ajax({
+        url: '/load',
+        type: 'post',
+        data: {
+            date: localStorage.getItem('date'),
+            email: localStorage.getItem('email')
+        },
+        success: (data) => {
+            console.log(data)
+            var foodlist = document.getElementById('foodlist')
+            var strengthlist = document.getElementById('strengthlist')
+            var cardiolist = document.getElementById('cardiolist')
 
-$.ajax({
-    url: '/load',
-    type: 'post', 
-    data: {
-        date: localStorage.getItem('date'),
-        email: localStorage.getItem('email')
-    },
-    success: (data) => {
-        console.log(data)
-        var foodlist = document.getElementById('foodlist')
-        var strengthlist = document.getElementById('strengthlist')
-        var cardiolist = document.getElementById('cardiolist')
+            for (var i = 0; i < data.foods.length; i++) {
+                var item = document.createElement('li')
+                item.className += " list-group-item"
+                item.appendChild(document.createTextNode(data.foods[i].name))
+                foodlist.appendChild(item)
+            }
 
-        for (var i = 0; i < data.foods.length; i++) {
-            var item = document.createElement('li')
-            item.className += " list-group-item"
-            item.appendChild(document.createTextNode(data.foods[i].name))
-            foodlist.appendChild(item)
+            for (var i = 0; i < data.cardio.length; i++) {
+                var item = document.createElement('li')
+                item.className += " list-group-item"
+                item.appendChild(document.createTextNode(data.cardio[i].name))
+                cardiolist.appendChild(item)
+            }
+
+
+            for (var i = 0; i < data.strength.length; i++) {
+                var item = document.createElement('li')
+                item.className += " list-group-item"
+                item.appendChild(document.createTextNode(data.strength[i].name))
+                strengthlist.appendChild(item)
+            }
+
+
         }
+    })
+}
 
-        for (var i = 0; i < data.cardio.length; i++) {
-            var item = document.createElement('li')
-            item.className += " list-group-item"
-            item.appendChild(document.createTextNode(data.cardio[i].name))
-            cardiolist.appendChild(item)
-        }
+loadData()
+
+var prevarrow = document.getElementById('prevarrow')
+var nextarrow = document.getElementById('nextarrow')
+var datespan = document.getElementById('datespan')
+function nextdate() {
+    var curDateStr = localStorage.getItem('date')
+    var curDate = new Date(curDateStr)
+    var nextDay = new Date(curDate)
+    nextDay.setDate(nextDay.getDate() + 1)
+    datespan.textContent = nextDay.toDateString()
+    localStorage.setItem('date', nextDay.toISOString())
+    $("ul").empty()
+    loadData()
+}
+
+function prevdate() {
+    var curDateStr = localStorage.getItem('date')
+    var curDate = new Date(curDateStr)
+    var prevDay = new Date(curDate)
+    prevDay.setDate(prevDay.getDate() - 1)
+    datespan.textContent = prevDay.toDateString()
+    localStorage.setItem('date', prevDay.toISOString())
+    $("ul").empty()
+    loadData()
+}
 
 
-        for (var i = 0; i < data.strength.length; i++) {
-            var item = document.createElement('li')
-            item.className += " list-group-item"
-            item.appendChild(document.createTextNode(data.strength[i].name))
-            strengthlist.appendChild(item)
-        }
-
-
-    }
-})
 
 

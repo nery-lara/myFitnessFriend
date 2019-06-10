7bbc890b576a1026dc904f4b3991c16cc35644f4
Angel Castro <angel90650@gmail.com>
2019-06-09 22:12:38 -0700

    strength exercises stored in db by form

--- routes/strength.js
+++ routes/strength.js
@@ -8,26 +8,25 @@ const Cardio = require('../models/cardio')
 
 
 module.exports = function (app) {
-    app.post('/food', (req, res) => {
+    app.post('/strength', (req, res) => {
         console.log(req.body)
-        const food = new Food({
+        const strength = new Strength({
             _id: new mongoose.Types.ObjectId(),
             name: req.body.name,
-            calories: req.body.calories,
-            protein: req.body.protein,
-            carbs: req.body.carbs,
-            fats: req.body.fats,
+            weight: req.body.weight,
+            sets: req.body.sets,
+            reps: req.body.reps,
             time: req.body.time,
         })
-        User.findOneAndUpdate({email: req.body.email}, {$push: {foods: food._id} }).exec().then(user => {
-           console.log(user)
-            if(user.length < 1) {
+        User.findOneAndUpdate({ email: req.body.email }, { $push: { 'exercises.strength': strength._id } }).exec().then(user => {
+            console.log(user)
+            if (user.length < 1) {
                 return res.status(401).json({
                     message: 'cant find user'
                 })
-            }else {
-                food.user = user._id
-                food.save().then(result => {
+            } else {
+                strength.user = user._id
+                strength.save().then(result => {
                     console.log(result)
                     res.status(201).render('pages/home')
                 }).catch(err => {
@@ -46,19 +45,18 @@ module.exports = function (app) {
         })
     })
 
-    app.delete("/food/:foodid",(req, res) => {
-        console.log(req.params.foodid)
-        Food.findOneAndDelete({_id: req.params.foodid}).exec().then(food => {
-            
-            User.findOneAndUpdate({_id: food.user}, {$pull: {foods: food._id}}).then(user => {
-                console.log("removed food from user")
-                
+    app.delete("/strength/:strengthid", (req, res) => {
+        console.log(req.params.strengthid)
+        Strength.findOneAndDelete({ _id: req.params.strengthid }).exec().then(strength => {
+
+            User.findOneAndUpdate({ _id: strength.user }, { $pull: { 'exercises.strength': strength._id } }).then(user => {
+                console.log("removed strength from user")
             }).catch(err => {
                 console.log('cant update user')
                 console.log(err)
             })
         }).catch(err => {
-            console.log('cant delete food')
+            console.log('cant delete strength')
             console.log(err)
         })
     })
